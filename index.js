464d368460de9115139da07736b89fbc2810a5b7
mythosmystery <axiysfire@gmail.com>
2021-10-13 22:48:40 -0500

    added map difference

--- index.js
+++ index.js
@@ -842,3 +842,91 @@ function findAndReplace(str, before, after) {
       .join(' ');
 }
 // console.log(findAndReplace('The quick brown fox jumped over the lazy dog', 'lazy', 'stinky'));
+
+////////////////////////////////////////////////////////
+
+class Set {
+   constructor() {
+      this.dictionary = {};
+      this.length = 0;
+   }
+
+   has(element) {
+      return this.dictionary[element] !== undefined;
+   }
+
+   values() {
+      return Object.keys(this.dictionary);
+   }
+
+   add(element) {
+      if (!this.has(element)) {
+         this.dictionary[element] = true;
+         this.length++;
+         return true;
+      }
+
+      return false;
+   }
+
+   remove(element) {
+      if (this.has(element)) {
+         delete this.dictionary[element];
+         this.length--;
+         return true;
+      }
+
+      return false;
+   }
+
+   size() {
+      return this.length;
+   }
+
+   union(set) {
+      const newSet = new Set();
+      this.values().forEach(value => {
+         newSet.add(value);
+      });
+      set.values().forEach(value => {
+         newSet.add(value);
+      });
+
+      return newSet;
+   }
+
+   intersection(set) {
+      const newSet = new Set();
+
+      let largeSet;
+      let smallSet;
+      if (this.dictionary.length > set.length) {
+         largeSet = this;
+         smallSet = set;
+      } else {
+         largeSet = set;
+         smallSet = this;
+      }
+
+      smallSet.values().forEach(value => {
+         if (largeSet.dictionary[value]) {
+            newSet.add(value);
+         }
+      });
+
+      return newSet;
+   }
+   difference(set) {
+      const newSet = new Set();
+
+      this.values().forEach(value => {
+         if (!set.dictionary[value]) {
+            newSet.add(value);
+         }
+      });
+
+      return newSet;
+   }
+}
+
+/////////////////////////////////////////
